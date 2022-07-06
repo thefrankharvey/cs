0c0ca3c9f1d337fa7070239a04bbd7c783cabfcd
Frank <frank@thefrankharvey.com>
2022-07-05 20:56:30 -0400

    make arrays

---
+++ data-structures/arrays/problems.js
@@ -0,0 +1,40 @@
+// more problems
+// https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/learn/lecture/12310382#overview
+
+// reverse a string
+
+function reverseString(str) {
+    let reversed = ''
+    // check input
+    if (!str || str.length < 2 || typeof str !== 'string') {
+        return 'pass a string with multiple letters'
+    }
+    for (i = str.length-1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+
+reverseString('hey')
+
+function reverse2(str) {
+    return str.split('').reverse().join('')
+}
+
+const reverse3 = str => [...str].reverse.join('')
+
+// merge sorted arrays
+
+array1 = [0,1,3,7,28]
+array2 = [2,5,9,30]
+
+function mergeArrays(arr1, arr2) {
+    // check input
+    // is array? is 2 params? 1 or both arrays empty?
+    let merged = [...arr1, ...arr2];
+    let sorted = merged.sort((a, b) => a - b)
+    return sorted
+}
+
+mergeArrays(array1, array2)
---
+++ data-structures/arrays/wiki.md
@@ -0,0 +1,6 @@
+## Arrays
+- [AlgoMonster](https://algo.monster/problems/binary_search_first_element_not_smaller_than_target)
+- [Udemy](https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/learn/lecture/12300322#overview)
+- [TechInterviewHandbook](https://www.techinterviewhandbook.org/algorithms/array/)
+- [Exponent](https://www.tryexponent.com/courses/software-engineering/data-structures/arrays)
+
